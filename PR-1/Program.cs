9f86699d1f0e4c7928f0fa0e15970ca92e5d6a88
Bhautik Ranpara <ranparabhautik30@gmail.com>
2026-03-26 19:29:33 +0530

    Add comment to Main method for clarity

--- PR-1/Program.cs
+++ PR-1/Program.cs
@@ -2,7 +2,7 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) // main method to initialise the program
         {
             Calculator c1 = new Calculator();
             c1.InitCalc();
